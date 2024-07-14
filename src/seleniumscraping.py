30bc0ff2153bd82e0fc52fbbe6380120ebbe367d
Roman <46465589+C0d3ing@users.noreply.github.com>
2024-07-14 02:10:10 -0700

    widnows

--- src/seleniumscraping.py
+++ src/seleniumscraping.py
@@ -29,7 +29,7 @@ def scraping():
     
     temp_dir = tempfile.mkdtemp()
     chrome_options = webdriver.ChromeOptions()
-    # # chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    #chrome_options.add_argument("--headless")  # Run Chrome in headless mode
     chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
     chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
     chrome_options.add_argument("--remote-debugging-port=9222")  # Enable remote debugging
