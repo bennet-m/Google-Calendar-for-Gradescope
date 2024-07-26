98acf0780257589002ec757754ff86baf00d883c
bennetdude <bennet.matazzoni@gmail.com>
2024-07-25 19:03:38 -0700

    new webdriver working yay

--- src/webdriverSetup.py
+++ src/webdriverSetup.py
@@ -2,21 +2,18 @@ import shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import certifi
-import urllib.request
-import ssl
 import logging
 import os
 import sys
 import stat
 from webdriver_manager.core.driver_cache import DriverCacheManager
 from macPath import *
+import chromedriver_autoinstaller
 
 logger = logging.getLogger(__name__)
 
 def get_driver():
     if sys.platform in ["Linux", "darwin"]:
-        import chromedriver_autoinstaller
         try:
             return chrome_driver_setup()
         except Exception as e:
@@ -54,55 +51,49 @@ def chrome_driver_setup():
             logger.warning(f"The file at {executable_path} is not executable. Attempting to fix permissions.")
             make_executable(executable_path)
     
-
     # I know this is disgusting. Please blame ChromeDriver and Pyinstaller with MacOS because they don't work well together
-    try:      
-        print("Attempting default chrome webdriver setup")
-        logger.info("Attempting default chrome webdriver setup")
-        driver = webdriver.Chrome(options=chrome_options)
+    try:
+        #Mac needs the apps resources to be in a specific location for .app applications
+        logger.info(f"Trying mac path method")
+        install_path = get_path()
+        
+        #Using Cache manager to specify path to be in the Mac Resource Path
+        cache_manager=DriverCacheManager(install_path)
+        
+        executable_path = ChromeDriverManager(cache_manager=cache_manager).install()
+        
+        third_party_notice_correction(executable_path)
+        
+        driver = webdriver.Chrome(service=ChromeService(executable_path), options=chrome_options)
         return driver
     except Exception as e:
         try: 
-            logger.info(f"Error. Attempting other default chrome webdriver setup: \n {e}")
-            service = ChromeService(ChromeDriverManager().install())
-            driver = webdriver.Chrome(service=service, options=chrome_options)
-        except Exception as e:
+            logger.info(f"Error. Attempting default chrome webdriver setup \n {e}")
+            driver = webdriver.Chrome(options=chrome_options)
+            return driver
+        except Exception as e: 
             try: 
-                logger.info(f"Error. Attempting autoinstaller: \n {e}")
-                chromedriver_autoinstaller.install()
-                driver = webdriver.Chrome()
-                return driver
-                
+                logger.info(f"Error. Attempting other default chrome webdriver setup: \n {e}")
+                service = ChromeService(ChromeDriverManager().install())
+                driver = webdriver.Chrome(service=service, options=chrome_options)
             except Exception as e:
-                try:
-                    logger.info(f"ChromeDriver not working. trying ChromeDriverManager().install() \n {e}")
-                    executable_path = ChromeDriverManager().install()
-                    third_party_notice_correction(executable_path)
-                    driver = webdriver.Chrome(options=chrome_options)
+                try: 
+                    logger.info(f"Error. Attempting autoinstaller: \n {e}")
+                    chromedriver_autoinstaller.install()
+                    driver = webdriver.Chrome()
                     return driver
-                
                 except Exception as e:
                     try:
-                        logger.info(f"ChromeDriver not working. Trying basic install for Selinium 4.11V \n {e}")
-                        driver = webdriver.Chrome()
+                        logger.info(f"ChromeDriver not working. trying ChromeDriverManager().install() \n {e}")
+                        executable_path = ChromeDriverManager().install()
+                        third_party_notice_correction(executable_path)
+                        driver = webdriver.Chrome(options=chrome_options)
                         return driver
-                
                     except Exception as e:
                         try:
-                            #Mac needs the apps resources to be in a specific location for .app applications
-                            logger.info(f"ChromeDriver not working. Trying Chad Mac Path Method \n {e}")
-                            install_path = get_path()
-                            
-                            #Using Cache manager to specify path to be in the Mac Resource Path
-                            cache_manager=DriverCacheManager(install_path)
-                            
-                            executable_path = ChromeDriverManager(cache_manager=cache_manager).install()
-                            
-                            third_party_notice_correction(executable_path)
-                            
-                            driver = webdriver.Chrome(service=ChromeService(executable_path), options=chrome_options)
+                            logger.info(f"ChromeDriver not working. Trying basic install for Selinium 4.11V \n {e}")
+                            driver = webdriver.Chrome()
                             return driver
-                            
                         except Exception as e:
                             logger.info(f"All Driver Initialization Attempts Failed \n {e}")
 
